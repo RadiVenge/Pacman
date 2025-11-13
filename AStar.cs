57e43db198a6b3a925a1e830b120bdc5a8c894a0
RadiVenge <vidar.felix2007@gmail.com>
2025-11-13 14:01:50 +0100

    finalized

--- AStar.cs
+++ AStar.cs
@@ -9,13 +9,13 @@ namespace Pacman
     {
         public static List<Vector2> FindPath(Vector2 start, Vector2 goal, Map map)
         {
-            var openSet = new List<AStarTile>();
-            var closedSet = new HashSet<Vector2>();
+            List<AStarTile> openSet = new List<AStarTile>();
+            HashSet<Vector2> closedSet = new HashSet<Vector2>();
             openSet.Add(new AStarTile(null, Vector2.Distance(start, goal), start));
 
             while (openSet.Count > 0)
             {
-                var current = LowestF(openSet);
+                AStarTile current = LowestF(openSet);
                 if (current == null)
                     break;
 
@@ -25,17 +25,17 @@ namespace Pacman
                 openSet.Remove(current);
                 closedSet.Add(current.Position);
 
-                foreach (var dir in new[] {
+                foreach (Vector2 dir in new[] {
                     new Vector2(0, -1), new Vector2(0, 1),
                     new Vector2(-1, 0), new Vector2(1, 0)
                 })
                 {
-                    var neighborPos = current.Position + dir;
+                    Vector2 neighborPos = current.Position + dir;
 
                     if (!map.IsWithinBounds(neighborPos) || !map[neighborPos].Walkable || closedSet.Contains(neighborPos))
                         continue;
 
-                    var existing = openSet.Find(t => SamePosition(t.Position, neighborPos));
+                    AStarTile existing = openSet.Find(t => SamePosition(t.Position, neighborPos));
                     float tentativeG = current.G + 1;
 
                     if (existing == null)
@@ -50,13 +50,13 @@ namespace Pacman
                 }
             }
 
-            return new List<Vector2>(); // No path found
+            return new List<Vector2>();
         }
 
         private static List<Vector2> ReconstructPath(AStarTile endTile)
         {
-            var path = new List<Vector2>();
-            var current = endTile;
+            List<Vector2> path = new List<Vector2>();
+            AStarTile current = endTile;
             while (current.Parent != null)
             {
                 path.Add(current.Position);
